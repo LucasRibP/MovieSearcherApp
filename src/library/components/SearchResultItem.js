de01cffd0fbfcddd77075443b6dcabdb3a2dee9b
Lucas <lrpessamilio@gmail.com>
2021-08-04 11:07:16 -0300

    Built basic SearchResultsItem

---
+++ src/library/components/SearchResultItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ListItem, Avatar } from "react-native-elements/dist/list/ListItem";
+
+export default SearchResultItem = ({ posterUrl, title, year }) => {
+  return (
+    <ListItem>
+      <Avatar source={posterUrl} />
+      <ListItem.Title>{title}</ListItem.Title>
+      <ListItem.Subtitle>{year}</ListItem.Subtitle>
+    </ListItem>
+  );
+};
--- src/screens/search/searchScreen.js
+++ src/screens/search/searchScreen.js
@@ -1,5 +1,6 @@
 import i18n from "i18n-js";
 import React from "react";
+import { FlatList } from "react-native";
 import { View, StyleSheet, Text, TextInput } from "react-native";
 import { Input, Button } from "react-native-elements";
 
@@ -10,7 +11,11 @@ export default SearchScreen = () => {
         placeholder={i18n.t("searchScreen.searchField.hint")}
         containerStyle={styles.inputContainer}
       />
-      <Button title={i18n.t("searchScreen.searchButton.text")} />
+      <Button
+        title={i18n.t("searchScreen.searchButton.text")}
+        containerStyle={styles.buttonContainer}
+      />
+      <FlatList style={styles.searchResultsContainer} />
     </View>
   );
 };
@@ -23,5 +28,12 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingHorizontal: 20,
     width: "100%",
+  },
+  buttonContainer: {
+    marginHorizontal: "3%",
+    width: "94%",
+  },
+  searchResultsContainer: {
+    margin: 5,
   },
 });
