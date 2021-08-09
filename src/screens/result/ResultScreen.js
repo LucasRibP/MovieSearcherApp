5e165ac2939d375701f78f22b5cc9bc27b44f091
Lucas <lrpessamilio@gmail.com>
2021-08-09 11:07:26 -0300

    Created Base for ResultScreen

---
+++ src/screens/result/ResultScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default ResultScreen = ({}) => {
+  return <View style={styles.screenContainer}></View>;
+};
+
+const styles = StyleSheet.create({
+  screenContainer: {},
+});
