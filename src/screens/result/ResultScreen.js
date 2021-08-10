c325af39343898e843ae0d5dd5e4de6fd8cc732b
Lucas <lrpessamilio@gmail.com>
2021-08-10 10:28:33 -0300

    Added poster to resultScreen

--- src/screens/result/ResultScreen.js
+++ src/screens/result/ResultScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import { Image } from "react-native-elements";
+
 import { useGetMovieByIMDbIDQuery } from "library/networking/omdbAPI";
 
 export default ResultScreen = ({ route }) => {
   const { imdbID } = route.params;
   const { data, error, isLoading } = useGetMovieByIMDbIDQuery(imdbID);
-  console.log(data);
+  console.log(data["Poster"]);
   return (
     <View style={styles.screenContainer}>
-      <Text>Movie Information</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: data["Poster"] }}
+          PlaceholderContent={<ActivityIndicator />}
+          style={styles.imageStyle}
+        />
+      </View>
     </View>
   );
 };
@@ -17,4 +25,59 @@ const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
   },
+  imageContainer: {
+    width: "100%",
+    height: "40%",
+  },
+  imageStyle: {
+    marginHorizontal: "27.5%",
+    marginTop: "5%",
+    width: "45%",
+    height: "90%",
+  },
 });
+
+/* Example data
+
+
+Object {
+  "Actors": "Sterling Holloway, Edward Brophy, Herman Bing",
+  "Awards": "Won 1 Oscar. 5 wins & 1 nomination total",
+  "BoxOffice": "N/A",
+  "Country": "United States",
+  "DVD": "13 Dec 2015",
+  "Director": "Samuel Armstrong, Norman Ferguson, Wilfred Jackson",
+  "Genre": "Animation, Adventure, Drama",
+  "Language": "English",
+  "Metascore": "96",
+  "Plot": "The stork delivers a baby elephant to Mrs. Jumbo, veteran of the circus, but the newborn is ridiculed because of his truly enormous ears and dubbed \"Dumbo\". After being separated from his mother, Dumbo is relegated to the circus' clown acts; it is up to his only friend, a mouse, to assist Dumbo to achieve his full potential.",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNWVmNWQ2OTQtNzJlNC00ZmQwLTg4ZTktZTNmM2IxZTlkOGM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+  "Production": "Walt Disney Productions",
+  "Rated": "G",
+  "Ratings": Array [
+    Object {
+      "Source": "Internet Movie Database",
+      "Value": "7.2/10",
+    },
+    Object {
+      "Source": "Rotten Tomatoes",
+      "Value": "98%",
+    },
+    Object {
+      "Source": "Metacritic",
+      "Value": "96/100",
+    },
+  ],
+  "Released": "31 Oct 1941",
+  "Response": "True",
+  "Runtime": "64 min",
+  "Title": "Dumbo",
+  "Type": "movie",
+  "Website": "N/A",
+  "Writer": "Joe Grant, Dick Huemer, Otto Englander",
+  "Year": "1941",
+  "imdbID": "tt0033563",
+  "imdbRating": "7.2",
+  "imdbVotes": "122,914",
+}
+*/
