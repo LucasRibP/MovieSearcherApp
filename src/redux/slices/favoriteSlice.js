0f63d8580d2538c74f929aeda4e1bb6febae90b3
Lucas <lrpessamilio@gmail.com>
2021-08-12 11:01:22 -0300

    Created favorites slice

---
+++ src/redux/slices/favoriteSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import _ from "lodash";
+
+export const favoriteSlice = createSlice({
+  name: "favorites",
+  initialState: {
+    value: [],
+  },
+  reducers: {
+    toggleFavorite: (state, id) => {
+      if (state.value.includes(id.payload)) {
+        state.value = _.remove(state.value, (item) => item == id.payload);
+      } else {
+        state.value.push(id.payload);
+      }
+    },
+  },
+});
+
+export const { toggleFavorite } = favoriteSlice.actions;
+
+export default favoriteSlice.reducer;
