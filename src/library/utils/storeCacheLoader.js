71f0cac969f29ee2f1e9c8075bdac18dce21cc89
Lucas <lrpessamilio@gmail.com>
2021-08-14 13:28:27 -0300

    Implemented saving functions

---
+++ src/library/utils/storeCacheLoader.js
@@ -0,0 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const storeFavorites = async (data) => {
+  try {
+    const jsonData = JSON.stringify(data);
+    await AsyncStorage.setItem("favorites", jsonData);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const retrieveFavorites = async (data) => {
+  try {
+    const jsonData = await AsyncStorage.getItem("favorites");
+    return jsonData != null ? JSON.parse(jsonData) : null;
+  } catch (e) {
+    console.log(e);
+  }
+};
