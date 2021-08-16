5331823caf1d1217b1cdc8ea6aaa6e7eb98096cc
Lucas <lrpessamilio@gmail.com>
2021-08-15 21:39:54 -0300

    Implemented favoritesScreen

--- src/screens/favorites/FavoritesScreen.js
+++ src/screens/favorites/FavoritesScreen.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 
 export default FavoritesScreen = () => {
-  return <View style={styles.componentContainer}></View>;
+  const favorites = useSelector((state) => state.favorites.value);
+  return (
+    <View style={styles.componentContainer}>
+      <ResultsList results={Object.values(favorites)} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   componentContainer: {
     flex: 1,
+    margin: 10,
   },
 });
