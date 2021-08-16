4b1da99716b1012f40fae5b5ce148c2f7207d1d7
Lucas <lrpessamilio@gmail.com>
2021-08-16 10:52:41 -0300

    Added title to resultScreen

--- src/screens/result/ResultInfo.js
+++ src/screens/result/ResultInfo.js
@@ -5,6 +5,7 @@ import { View, StyleSheet, Text } from "react-native";
 export default ResultInfo = ({ data }) => {
   return (
     <View style={styles.componentContainer}>
+      <Text style={styles.titleText}>{data["Title"]}</Text>
       <Text style={styles.genreText}>{data["Genre"]}</Text>
       <Text style={styles.releaseText}>
         {i18n.t("resultScreen.releaseText")}
@@ -23,6 +24,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
   },
+  titleText: { fontWeight: "bold", fontSize: 24, marginBottom: 10 },
   genreText: { fontWeight: "bold" },
   plotTextContainer: { marginHorizontal: "5%", marginTop: "3%" },
   plotText: {},
